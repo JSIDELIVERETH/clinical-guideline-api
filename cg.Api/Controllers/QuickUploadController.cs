985e6da63c406262474f4988646eddb178653eec
yohe2111 <yohe2111@gmail.com>
2019-02-22 14:17:16 +0300

    name uploaded file based on node id

--- cg.Api/Controllers/QuickUploadController.cs
+++ cg.Api/Controllers/QuickUploadController.cs
@@ -17,7 +17,9 @@ namespace cg.Api.Controllers
     [Route("api/QuickUpload")]
     public class QuickUploadController : Controller
     {
-        private static int DefaultBufferSize = 80 * 1024;
+        private static string _uploadDirectory = "assets/images";
+        private static string _defaultURLPath = "http://35.184.137.237:8915/MyImages/";
+        private static int _defaultBufferSize = 80 * 1024;
 
         public QuickUploadController()
         {
@@ -40,16 +42,33 @@ namespace cg.Api.Controllers
                 Directory.CreateDirectory(directory);
             }
             
-            var fileName = Path.GetFileName(upload.FileName);
+            var fileName = CustomizeFileName(upload.FileName);
 
             using (var fileStream = new FileStream(Path.Combine(directory, $"{fileName}"), FileMode.Create))
             {
                 var inputStream = upload.OpenReadStream();
-                await inputStream.CopyToAsync(fileStream, DefaultBufferSize, cancellationToken);
+                await inputStream.CopyToAsync(fileStream, _defaultBufferSize, cancellationToken);
             }
 
-            var url = Path.Combine("http://localhost:5001/MyImages/", fileName);
+            var url = Path.Combine(_defaultURLPath, fileName);
             return new JsonResult(new { fileName = fileName, uploaded = 1, url = url, error = new { } });
+        }
+
+
+        private string CustomizeFileName(string fileName)
+        {
+            int fileTagNo = 1;
+            int index = fileName.LastIndexOf('.');
+            string extension = fileName.Substring(index);
+            string name = fileName.Substring(0, index);
+
+            fileName = name + "-" + fileTagNo + extension;
+            while (System.IO.File.Exists(Path.Combine(_uploadDirectory, $"{fileName}")))
+            {
+                fileName = name + "-" + fileTagNo + extension;
+                fileTagNo++;
+            }
+            return fileName;
         }
     }
 }
